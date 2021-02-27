73eb1a801c92a453655715955bfc736c1fa1b8f9
EdManDev <ed.manigat@gmail.com>
2021-02-26 22:01:53 -0400

    examen test of Number of Points

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a17_sist_7_010;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        int math = 5;
+        int quimica = 5;
+        int fisica = 0;
+        int promedio = 0;
+
+        promedio = (math + quimica + fisica ) / 3;
+        
+        if (promedio <= 5) {
+            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
+        }
+        else if (promedio <= 5){
+
+            Toast.makeText(this, "Reaprobado", Toast.LENGTH_SHORT).show();
+            
+        }
+    } 
+
+
+
 }
