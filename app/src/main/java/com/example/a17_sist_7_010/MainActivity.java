2117944faa4e7d83799b9ed4efa34466f3af945f
EdManDev <ed.manigat@gmail.com>
2021-02-27 15:59:06 -0400

    calculator with radio Button but wi Have Multiplication and Division

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -6,41 +6,64 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1, et2;
-    private TextView tv1;
-    private RadioButton rb1, rb2;
+    private EditText et_valor1, et_valor2;
+    private TextView tv_resultado;
+    private RadioButton rb_suma, rb_resta, rb_mul, rb_div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.txt_valor1);
-        et2 = (EditText)findViewById(R.id.txt_valor2);
-        tv1 = (TextView)findViewById(R.id.txt_resultado);
-        rb1 = (RadioButton)findViewById(R.id.rb_sumar);
-        rb2 = (RadioButton)findViewById(R.id.rb_restar);
+        et_valor1 = (EditText)findViewById(R.id.txt_valor1);
+        et_valor2 = (EditText)findViewById(R.id.txt_valor2);
+        rb_suma = (RadioButton)findViewById(R.id.rb_sumar);
+        rb_resta = (RadioButton)findViewById(R.id.rb_restar);
+        rb_mul = (RadioButton)findViewById(R.id.rb_multiplication);
+        rb_div = (RadioButton)findViewById(R.id.rb_division);
+        tv_resultado = (TextView)findViewById(R.id.txt_resultado);
     }
 
     // Methodo para el button calcular
     public void Calcular (View view) {
-        String valor1_String = et1.getText().toString();
-        String valor2_String = et2.getText().toString();
+        String valor1_String = et_valor1.getText().toString();
+        String valor2_String = et_valor2.getText().toString();
 
         int valor1_int = Integer.parseInt(valor1_String);
         int valor2_int = Integer.parseInt(valor2_String);
 
-        if (rb1.isChecked() == true ) {
+        if (rb_suma.isChecked() == true ) {
             int suma = valor1_int + valor2_int;
+
             String resultado = String.valueOf(suma);
-            tv1.setText(resultado);
-        } else if(rb2.isChecked() == true) {
+            tv_resultado.setText(resultado);
+        }
+        else if(rb_resta.isChecked() == true) {
             int resta = valor1_int - valor2_int;
+
             String resultado = String.valueOf(resta);
-            tv1.setText(resultado);
+            tv_resultado.setText(resultado);
+        }
+        else if(rb_mul.isChecked() == true) {
+            int multiplication = valor1_int * valor2_int;
+
+            String resultado = String.valueOf(multiplication);
+            tv_resultado.setText(resultado);
+        }
+        else if(rb_div.isChecked() == true) {
+
+            if (valor2_int != 0 ) {
+                int division = valor1_int / valor2_int;
+                String resultado = String.valueOf(division);
+                tv_resultado.setText(resultado);
+
+            } else {
+                Toast.makeText(this, "El Segundo valor debe ser diferente de cero", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
