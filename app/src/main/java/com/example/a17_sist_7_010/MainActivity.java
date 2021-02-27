bce2a5810198ea6b9b575b9a279813ab2698a0e4
EdManDev <ed.manigat@gmail.com>
2021-02-27 18:16:28 -0400

    *calculator with  Control Spinner or DropDown Menu(Text Size Improved)

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         String [] opciones = {"sumar", "restar", "multiplicar", "dividir"};
 
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_item, opciones );
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, R.layout.spinner_item, opciones );
         spinner1.setAdapter(adapter);
     }
 
