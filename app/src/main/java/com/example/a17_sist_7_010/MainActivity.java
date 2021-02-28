a18cf411f7f91db6e7455fe5aae13f8e46ea1054
EdManDev <ed.manigat@gmail.com>
2021-02-27 22:40:11 -0400

    2 Activity and past Variable and back to the start

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -1,70 +1,28 @@
 package com.example.a17_sist_7_010;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner1;
-    private EditText et1, et2;
-    private TextView tv1;
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.txt_valor1);
-        et2 = (EditText)findViewById(R.id.txt_valor2);
-        tv1 = (TextView) findViewById(R.id.tv_resultado);
-        spinner1 = (Spinner) findViewById(R.id.spinner);
-
-        String [] opciones = {"sumar", "restar", "multiplicar", "dividir"};
-
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, R.layout.spinner_item, opciones );
-        spinner1.setAdapter(adapter);
+        et1 = (EditText)findViewById(R.id.et1);
     }
 
-    // Methodo para el button calcular
-    public void Calcular (View view) {
-        String valor1_String = et1.getText().toString();
-        String valor2_String = et2.getText().toString();
-
-        int valor1_int = Integer.parseInt(valor1_String);
-        int valor2_int = Integer.parseInt(valor2_String);
-
-        String seleccion = spinner1.getSelectedItem().toString();
-
-        if (seleccion.equals("sumar")) {
-            int suma = valor1_int + valor2_int;
-            String resultado = String.valueOf(suma);
-            tv1.setText(resultado);
-        }
-        else if (seleccion.equals("restar")){
-            int restar = valor1_int - valor2_int;
-            String resultado = String.valueOf(restar);
-            tv1.setText(resultado);
-        }
-        else if (seleccion.equals("multiplicar")){
-            int mult = valor1_int * valor2_int;
-            String resultado = String.valueOf(mult);
-            tv1.setText(resultado);
-        }
-        else if (seleccion.equals("dividir")){
-            if (valor2_int !=0) {
-                int divi = valor1_int / valor2_int;
-                String resultado = String.valueOf(divi);
-                tv1.setText(resultado);
-            }else {
-                Toast.makeText(this, "No se Puede dividir entre Cero", Toast.LENGTH_LONG).show();
-            }
-        }
+    //Método para el botón enviar
+    public void Enviar (View view){
+        Intent i = new Intent(this, Main2Activity.class);
+        i.putExtra("dato", et1.getText().toString());
+        startActivity(i);
     }
 }
