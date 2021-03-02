d23d8ee6b59ac2b160d5e1699315ab69bb7f594a
EdManDev <ed.manigat@gmail.com>
2021-03-02 10:25:25 -0400

    Programation Button Jugar

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -50,5 +52,32 @@ public class MainActivity extends AppCompatActivity {
             iv_personaje.setImageResource(id);
         }
 
+        mp = MediaPlayer.create(this, R.raw.alphabet_song);
+        mp.start();
+        mp.setLooping(true);
     }
+
+    public void Jugar(View view) {
+        String nombre = et_nombre.getText().toString();
+        if (!nombre.equals("")) {
+            mp.stop();
+            mp.release();
+            Intent intent = new Intent(this, Main2Activity_Nivel1.class);
+
+            intent.putExtra("jugador", nombre);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            Toast.makeText(this, "Primero debes escribir tu nombre", Toast.LENGTH_SHORT).show();
+
+            et_nombre.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+//    @Override
+//    public void onBackPressed(){
+//          Toast.makeText(this, "Bye 17-SIST-7-10", Toast.LENGTH_SHORT).show();
+//    }
 }
