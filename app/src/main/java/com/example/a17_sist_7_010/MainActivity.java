6710237957c20502f7d59d4f8713a57e49d209be
EdManDev <ed.manigat@gmail.com>
2021-03-02 09:18:26 -0400

    implementacion image View and Splash Screen

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -2,27 +2,53 @@ package com.example.a17_sist_7_010;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1;
+    private EditText et_nombre;
+    private ImageView iv_personaje;
+    private TextView tv_bestScore;
+    private MediaPlayer mp;
+
+    int num_aleatorio = (int) (Math.random() * 10);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.et1);
-    }
+        et_nombre = (EditText) findViewById(R.id.txt_nombre);
+        iv_personaje = (ImageView)findViewById(R.id.imageView_Personaje);
+        tv_bestScore = (TextView) findViewById(R.id.textView_BestScore);
 
-    //Método para el botón enviar
-    public void Enviar (View view){
-        Intent i = new Intent(this, Main2Activity.class);
-        i.putExtra("dato", et1.getText().toString());
-        startActivity(i);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        int id;
+        if(num_aleatorio == 0 || num_aleatorio == 10){
+            id = getResources().getIdentifier("mango", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if(num_aleatorio == 1 || num_aleatorio == 9){
+            id = getResources().getIdentifier("fresa", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if(num_aleatorio == 2 || num_aleatorio == 8){
+            id = getResources().getIdentifier("manzana", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if(num_aleatorio == 3 || num_aleatorio == 7){
+            id = getResources().getIdentifier("sandia", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if(num_aleatorio == 4 || num_aleatorio == 5 || num_aleatorio == 6){
+            id = getResources().getIdentifier("uva", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        }
+
     }
 }
