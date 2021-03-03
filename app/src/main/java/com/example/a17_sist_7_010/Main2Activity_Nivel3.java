0e8cd112e84ee776d72507a53139d68f987eb3e2
EdManDev <ed.manigat@gmail.com>
2021-03-03 16:25:44 -0400

    Alta, modificación y consulta del score (FIXED BUG) Done

---
+++ app/src/main/java/com/example/a17_sist_7_010/Main2Activity_Nivel3.java
@@ -0,0 +1,14 @@
+package com.example.a17_sist_7_010;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity_Nivel3 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main2__nivel3);
+  }
+}
