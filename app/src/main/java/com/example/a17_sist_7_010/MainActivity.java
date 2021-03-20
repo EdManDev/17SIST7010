f934e83781db1e3c9c8c1fbb061d103378a8d664
EdManDev <ed.manigat@gmail.com>
2021-03-20 03:58:13 -0400

    Grabación de vídeo en Android Studio Done

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -1,101 +1,33 @@
 package com.example.a17_sist_7_010;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.media.MediaPlayer;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    private EditText et_nombre;
-    private ImageView iv_personaje;
-    private TextView tv_bestScore;
-    private MediaPlayer mp;
-
-    int num_aleatorio = (int) (Math.random() * 10);
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        et_nombre = (EditText) findViewById(R.id.txt_nombre);
-        iv_personaje = (ImageView)findViewById(R.id.imageView_Personaje);
-        tv_bestScore = (TextView) findViewById(R.id.textView_BestScore);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
-
-        int id;
-        if(num_aleatorio == 0 || num_aleatorio == 10){
-            id = getResources().getIdentifier("mango", "drawable", getPackageName());
-            iv_personaje.setImageResource(id);
-        } else if(num_aleatorio == 1 || num_aleatorio == 9){
-            id = getResources().getIdentifier("fresa", "drawable", getPackageName());
-            iv_personaje.setImageResource(id);
-        } else if(num_aleatorio == 2 || num_aleatorio == 8){
-            id = getResources().getIdentifier("manzana", "drawable", getPackageName());
-            iv_personaje.setImageResource(id);
-        } else if(num_aleatorio == 3 || num_aleatorio == 7){
-            id = getResources().getIdentifier("sandia", "drawable", getPackageName());
-            iv_personaje.setImageResource(id);
-        } else if(num_aleatorio == 4 || num_aleatorio == 5 || num_aleatorio == 6){
-            id = getResources().getIdentifier("uva", "drawable", getPackageName());
-            iv_personaje.setImageResource(id);
-        }
-
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "BD" , null, 1);
-        SQLiteDatabase BD = admin.getReadableDatabase();
-
-        Cursor consulta = BD.rawQuery("SELECt * FROM puntaje WHERE score = (SELECT max(score) FROM puntaje) ", null);
-        if (consulta.moveToFirst()) {
-            String temp_nombre = consulta.getString(0);
-            String temp_score = consulta.getString(1);
-            tv_bestScore.setText("Record:"+ temp_score + " de: " + temp_nombre);
-            BD.close();
-        }else {
-            BD.close();
-        }
-
-        mp = MediaPlayer.create(this, R.raw.alphabet_song);
-        mp.start();
-        mp.setLooping(true);
+    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
     }
+  }
 
-    public void Jugar(View view) {
-        String nombre = et_nombre.getText().toString();
-        if (!nombre.equals("")) {
-            mp.stop();
-            mp.release();
-            Intent intent = new Intent(this, Main2Activity_Nivel1.class);
+  static final int REQUEST_VIDEO_CAPTURE = 1;
 
-            intent.putExtra("jugador", nombre);
-            startActivity(intent);
-            finish();
-        }
-        else {
-            Toast.makeText(this, "Primero debes escribir tu nombre", Toast.LENGTH_SHORT).show();
-
-            et_nombre.requestFocus();
-            InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
-            imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
-        }
+  public void TomarVideo(View view) {
+    Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+    if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+      startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
     }
-
-    @Override
-    public void onBackPressed(){
-        finish();
-        mp.stop();
-        mp.release();
-        Toast.makeText(this, "Bye 17-SIST-7-10", Toast.LENGTH_SHORT).show();
-    }
+  }
 }
