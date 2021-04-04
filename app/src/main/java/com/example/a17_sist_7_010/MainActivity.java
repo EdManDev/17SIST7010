a55a7f60a2a32514e17cbdd2f177531dcb0d7d10
EdManDev <ed.manigat@gmail.com>
2021-04-03 21:58:00 -0400

    Almacenamiento de datos - La clase SharedPreferences DONE

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.a17_sist_7_010;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 
@@ -21,13 +17,18 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    et1 = (EditText)findViewById(R.id.txt_web);
+    et1 = (EditText)findViewById(R.id.txt_mail);
+
+    SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+    et1.setText(preferences.getString("mail", ""));
   }
 
-  // Methodo boton ir
-  public void Navegar (View view) {
-    Intent i = new Intent(this, MainActivity2.class);
-    i.putExtra("sitioWeb", et1.getText().toString());
-    startActivity(i);
+  //  Methodo para el Button Guardar
+  public void Guardar(View view){
+    SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+    SharedPreferences.Editor Obj_editor = preferences.edit();
+    Obj_editor.putString("mail", et1.getText().toString());
+    Obj_editor.commit();
+    finish();
   }
 }
