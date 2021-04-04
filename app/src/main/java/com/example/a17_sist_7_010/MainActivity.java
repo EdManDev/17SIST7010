1c6e073700d6ac2b71b1891338471bb8f3fae69d
EdManDev <ed.manigat@gmail.com>
2021-04-04 16:15:34 -0400

    Almacenamiento de datos externo  done

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -4,67 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-
 public class MainActivity extends AppCompatActivity {
 
-  private EditText et1;
+  private EditText et_nombre, et_contenido;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    et1 = (EditText)findViewById(R.id.txt_bitacora);
-    String archivos [] = fileList();
+    et_nombre = (EditText)findViewById(R.id.txt_nombre);
+    et_contenido = (EditText)findViewById(R.id.txt_contenido);
+  }
 
-    if (ArchivoExist(archivos, "bitacora.txt")){
-        try {
-          InputStreamReader archivo = new InputStreamReader(openFileInput("bitacora.txt"));
-          BufferedReader br = new BufferedReader(archivo);
-          String linea = br.readLine();
-          String bitacoraCompleta = "";
+  //Método para el botón Guardar
+  public void Guardar(View view){
+    String nombre = et_nombre.getText().toString();
+    String contenido = et_contenido.getText().toString();
 
-          while(linea != null) {
-            bitacoraCompleta = bitacoraCompleta + linea + "\n";
-            linea = br.readLine();
-          }
-          br.close();
-          archivo.close();
-          et1.setText(bitacoraCompleta);
-        } catch (IOException e) {
+    try {
+      File tarjetaSD = Environment.getExternalStorageDirectory();
+      Toast.makeText(this, tarjetaSD.getPath(), Toast.LENGTH_SHORT).show();
+      File rutaArchivo = new File(tarjetaSD.getPath(), nombre);
+      OutputStreamWriter crearArchivo = new OutputStreamWriter(openFileOutput(nombre, Activity.MODE_PRIVATE));
 
-        }
+      crearArchivo.write(contenido);
+      crearArchivo.flush();
+      crearArchivo.close();
+
+      Toast.makeText(this, "Guardado correctamente", Toast.LENGTH_SHORT).show();
+      et_nombre.setText("");
+      et_contenido.setText("");
+    }catch (IOException e){
+      Toast.makeText(this, "No se pudo guardar", Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  //Método consultar
+  public void Consultar(View view){
+    String nombre = et_nombre.getText().toString();
+
+    try{
+      File tarjetaSD = Environment.getExternalStorageDirectory();
+      File rutaArchivo = new File(tarjetaSD.getPath(), nombre);
+      InputStreamReader abrirArchivo = new InputStreamReader(openFileInput(nombre));
+
+      BufferedReader leerArchivo = new BufferedReader(abrirArchivo);
+      String linea = leerArchivo.readLine();
+      String contenidoCompleto = "";
+
+      while (linea != null){
+        contenidoCompleto = contenidoCompleto + linea + "\n";
+        linea = leerArchivo.readLine();
       }
+
+      leerArchivo.close();
+      abrirArchivo.close();
+      et_contenido.setText(contenidoCompleto);
+    }catch (IOException e){
+      Toast.makeText(this, "Error al leer el archivo", Toast.LENGTH_SHORT).show();
     }
-
-    private boolean ArchivoExist(String archivos [], String NombreArchivo) {
-    for(int i = 0 ; i < archivos.length; i++)
-      if (NombreArchivo.equals(archivos[i]))
-        return true;
-      return false;
-    }
-
-    // METHODE PARA BUTTON GUARDAR
-    public void Guardar (View view) {
-      try {
-          OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("bitacora.txt", Activity.MODE_PRIVATE));
-          archivo.write(et1.getText().toString());
-          archivo.flush();
-          archivo.close();
-
-      } catch (IOException e) {
-
-      }
-      Toast.makeText(this, "BItacora guardada correctamente", Toast.LENGTH_SHORT).show();
-      finish();
-    }
+  }
 }
