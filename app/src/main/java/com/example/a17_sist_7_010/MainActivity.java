9f77609cac2524520cc423831832ba556e274b71
EdManDev <ed.manigat@gmail.com>
2021-04-05 11:38:44 -0400

    Base de datos – Modificaciones  done

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
   // METHODO PARA GUARDAR PRODUCTO
   public void Registrar(View view) {
-    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "admintracion", null, 1);
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
     SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
 
     String codigo = et_codigo.getText().toString();
@@ -35,40 +35,41 @@ public class MainActivity extends AppCompatActivity {
 
     if (!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()) {
       ContentValues registro = new ContentValues();
+
       registro.put("codigo", codigo);
       registro.put("descripcion", descripcion);
       registro.put("precio", precio);
 
       BaseDeDatos.insert("articulos", null, registro);
-      BaseDeDatos.close();
 
+      BaseDeDatos.close();
       et_codigo.setText("");
       et_descripcion.setText("");
       et_precio.setText("");
 
-      Toast.makeText(this, "registro exitoso", Toast.LENGTH_SHORT).show();
+      Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
     } else {
       Toast.makeText(this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
     }
   }
 
   // METHODE PARA CONSULTAR UN ARTICULO O PROUCTO
-  public void Buscar(View view){
+  public void Buscar(View view) {
     AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
     SQLiteDatabase BaseDeDatabase = admin.getWritableDatabase();
 
     String codigo = et_codigo.getText().toString();
 
-    if(!codigo.isEmpty()){
+    if (!codigo.isEmpty()) {
       Cursor fila = BaseDeDatabase.rawQuery
               ("select descripcion, precio from articulos where codigo =" + codigo, null);
 
-      if(fila.moveToFirst()){
+      if (fila.moveToFirst()) {
         et_descripcion.setText(fila.getString(0));
         et_precio.setText(fila.getString(1));
         BaseDeDatabase.close();
       } else {
-        Toast.makeText(this,"No existe el artículo", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "No existe el artículo", Toast.LENGTH_SHORT).show();
         BaseDeDatabase.close();
       }
 
@@ -78,28 +79,60 @@ public class MainActivity extends AppCompatActivity {
   }
 
   // METHODO PARA ELIMINAR UN PRODUCTO o ARTICULO
-  public  void Eliminar (View view) {
-    AdminSQLiteOpenHelper admin =  new AdminSQLiteOpenHelper
-            (this, "administracion", null,1);
-    SQLiteDatabase BaseDeDatabase = admin.getWritableDatabase();
+  public void Eliminar(View view) {
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper
+            (this, "administracion", null, 1);
+    SQLiteDatabase BaseDatabase = admin.getWritableDatabase();
+
     String codigo = et_codigo.getText().toString();
 
     if (!codigo.isEmpty()) {
-       int cantidad = BaseDeDatabase.delete("articulos", "codigo" + codigo, null);
-       BaseDeDatabase.close();
 
-       et_codigo.setText("");
-       et_descripcion.setText("");
-       et_precio.setText("");
-       
-       if (cantidad == 1  ){
-         Toast.makeText(this, "Articulo eliminando exitosamente", Toast.LENGTH_SHORT).show();
-       }else{
-         Toast.makeText(this, "El articulo no existe", Toast.LENGTH_SHORT).show();
-       }
-       
-    }else{
-      Toast.makeText(this, "Debes introducir el coigo del author", Toast.LENGTH_SHORT).show();
+      int cantidad = BaseDatabase.delete("articulos", "codigo=" + codigo, null);
+      BaseDatabase.close();
+
+      et_codigo.setText("");
+      et_descripcion.setText("");
+      et_precio.setText("");
+
+      if (cantidad == 1) {
+        Toast.makeText(this, "Artículo eliminado exitosamente", Toast.LENGTH_SHORT).show();
+      } else {
+        Toast.makeText(this, "El artículo no existe", Toast.LENGTH_SHORT).show();
+      }
+
+    } else {
+      Toast.makeText(this, "Debes de introducir el código del artículo", Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  // METHODO MODIFICAR ARTICULO O PRODUCTO
+  public void Modificar(View view) {
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+    SQLiteDatabase BaseDatabase = admin.getWritableDatabase();
+
+    String codigo = et_codigo.getText().toString();
+    String descripcion = et_descripcion.getText().toString();
+    String precio = et_precio.getText().toString();
+
+    if (!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()) {
+
+      ContentValues registro = new ContentValues();
+      registro.put("codigo", codigo);
+      registro.put("descripcion", descripcion);
+      registro.put("precio", precio);
+
+      int cantidad = BaseDatabase.update("articulos", registro, "codigo=" + codigo, null);
+      BaseDatabase.close();
+
+      if (cantidad == 1) {
+        Toast.makeText(this, "Artículo modificado correctamente", Toast.LENGTH_SHORT).show();
+      } else {
+        Toast.makeText(this, "El artículo no existe", Toast.LENGTH_SHORT).show();
+      }
+
+    } else {
+      Toast.makeText(this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
     }
   }
 }
