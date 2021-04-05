8045bf8e70043a0a65c01732161bb95b03742fcf
EdManDev <ed.manigat@gmail.com>
2021-04-04 21:52:42 -0400

    Base de datos - Altas (SQLite) en Android done

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -2,78 +2,52 @@ package com.example.a17_sist_7_010;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 public class MainActivity extends AppCompatActivity {
 
-  private EditText et_nombre, et_contenido;
+  private EditText et_codigo, et_descripcion, et_precio;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    et_nombre = (EditText)findViewById(R.id.txt_nombre);
-    et_contenido = (EditText)findViewById(R.id.txt_contenido);
+    et_codigo = (EditText)findViewById(R.id.txt_codigo);
+    et_descripcion = (EditText)findViewById(R.id.txt_descripcion);
+    et_precio = (EditText)findViewById(R.id.txt_precio);
   }
 
-  //Método para el botón Guardar
-  public void Guardar(View view){
-    String nombre = et_nombre.getText().toString();
-    String contenido = et_contenido.getText().toString();
+  // METHODO PARA GUARDAR PRODUCTO
+  public void Registrar(View view) {
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "admintracion", null, 1);
+    SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
 
-    try {
-      File tarjetaSD = Environment.getExternalStorageDirectory();
-      Toast.makeText(this, tarjetaSD.getPath(), Toast.LENGTH_SHORT).show();
-      File rutaArchivo = new File(tarjetaSD.getPath(), nombre);
-      OutputStreamWriter crearArchivo = new OutputStreamWriter(openFileOutput(nombre, Activity.MODE_PRIVATE));
+    String codigo = et_codigo.getText().toString();
+    String descripcion = et_descripcion.getText().toString();
+    String precio = et_precio.getText().toString();
 
-      crearArchivo.write(contenido);
-      crearArchivo.flush();
-      crearArchivo.close();
+    if (!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
+      ContentValues registro =  new ContentValues();
+      registro.put("codigo", codigo);
+      registro.put("descripcion", descripcion);
+      registro.put("precio", precio);
 
-      Toast.makeText(this, "Guardado correctamente", Toast.LENGTH_SHORT).show();
-      et_nombre.setText("");
-      et_contenido.setText("");
-    }catch (IOException e){
-      Toast.makeText(this, "No se pudo guardar", Toast.LENGTH_SHORT).show();
-    }
-  }
+      BaseDeDatos.insert("articulos", null, registro);
+      BaseDeDatos.close();
 
-  //Método consultar
-  public void Consultar(View view){
-    String nombre = et_nombre.getText().toString();
+      et_codigo.setText("");
+      et_descripcion.setText("");
+      et_precio.setText("");
 
-    try{
-      File tarjetaSD = Environment.getExternalStorageDirectory();
-      File rutaArchivo = new File(tarjetaSD.getPath(), nombre);
-      InputStreamReader abrirArchivo = new InputStreamReader(openFileInput(nombre));
-
-      BufferedReader leerArchivo = new BufferedReader(abrirArchivo);
-      String linea = leerArchivo.readLine();
-      String contenidoCompleto = "";
-
-      while (linea != null){
-        contenidoCompleto = contenidoCompleto + linea + "\n";
-        linea = leerArchivo.readLine();
-      }
-
-      leerArchivo.close();
-      abrirArchivo.close();
-      et_contenido.setText(contenidoCompleto);
-    }catch (IOException e){
-      Toast.makeText(this, "Error al leer el archivo", Toast.LENGTH_SHORT).show();
+      Toast.makeText(this, "registro exitoso", Toast.LENGTH_SHORT).show();
+    }else {
+      Toast.makeText(this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
     }
   }
 }
