e2a5ccabc0d7ad89d4967a060da01b3e7aaaebee
EdManDev <ed.manigat@gmail.com>
2021-04-11 22:47:57 -0400

    Reproducción de audio DONE

--- app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
+++ app/src/main/java/com/example/a17_sist_7_010/MainActivity.java
@@ -2,59 +2,35 @@ package com.example.a17_sist_7_010;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+  Button play;
+  SoundPool sp;
+  int sonido_de_reproducion;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    play = (Button) findViewById(R.id.button_play);
+    sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 1);
+    sonido_de_reproducion = sp.load(this, R.raw.sound_short, 1);
   }
 
-  public void Selection(View view) {
-    switch (view.getId()) {
-      case R.id.fresas:
-        Toast.makeText(this, "Estas son Fresas", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.manzana:
-        Toast.makeText(this, "Estas son manzanas", Toast.LENGTH_SHORT).show();;
-        break;
-      case R.id.pina:
-        Toast.makeText(this, "Estas son Pinas", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.frambuesas:
-        Toast.makeText(this, "Estas son frambuesas", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.kiwis:
-        Toast.makeText(this, "Estas son kiwis", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.mangos:
-        Toast.makeText(this, "Estas son mangos", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.naranjas:
-        Toast.makeText(this, "Estas son naranjas", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.sandia:
-        Toast.makeText(this, "Estas son sandia", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.uvas:
-        Toast.makeText(this, "Estas son uvas", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.zarzamora:
-        Toast.makeText(this, "Estas son zarzamora", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.bananas:
-        Toast.makeText(this, "Estas son bananas", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.pera:
-        Toast.makeText(this, "Estas son pera", Toast.LENGTH_SHORT).show();
-        break;
-      case R.id.melon:
-        Toast.makeText(this, "Estas son melon", Toast.LENGTH_SHORT).show();
-        break;
-    }
+  public void AudioSoundPool(View view) {
+    sp.play(sonido_de_reproducion, 1, 1,1,0, 0);
+  }
+  public void AudioMediaPlayer (View view) {
+    MediaPlayer mp = MediaPlayer.create(this, R.raw.fabrice_rouzier_g_);
+    mp.start();
   }
 }
